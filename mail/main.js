fae11bae35390a0007e8d4a1505112a8e1f16868
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-15 15:20:25 +0000

    Passettini lentini lentini

---
+++ mail/main.js
@@ -0,0 +1,16 @@
+// SECTION Variabili globali
+
+ let emailInput = ["a@x", "b@x", "c@x", "d@x", "e@x" ];
+ let email = prompt ("Dimmi la tua Email");
+
+// SECTION Variabili globali FINE
+let text=""
+for (i = 1; i < emailInput.length;  i++) {
+  text += emailInput[i] + "<br>";
+
+//    if (email == emailInput[i]){
+//     document.getElementById(output).innerHTML = "Benvenuto"
+// }
+
+
+ }
